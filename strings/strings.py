6a40c842a3afa8386467610afda694803be940ba
bmartinez <bryan.martinez@quetzalio.net>
2021-08-08 13:51:01 -0700

    clase 2 de Alan

---
+++ strings/strings.py
@@ -0,0 +1,51 @@
+#\n vuelta de carro o nuevo renglon \ se llama escape
+# print("La jirafa es amarilla \ny Manchada")
+
+# print("Este maestro es muy \"manchado\" ")
+
+frase = "El perico es verde"
+frase2 = " y come semillas"
+
+# print(frase + frase2)
+
+#funcion upper(): hace que el string de una variable se convierta en mayusculas
+
+# print(frase.upper() + frase2.upper())
+
+#funcion isupper(): compara el string de una variable para saber si son mayusculas
+
+#frase verdadera, frase2 falso
+
+# print(frase.upper().isupper() + frase2.isupper())
+
+# #1 es verdadero
+# #0 es falso
+
+# #frase verdadera, frase2 verdadera
+
+# print(frase.upper().isupper() + frase2.upper().isupper())
+
+# #frase falso, frase2 falso
+
+
+# print(frase.isupper() + frase2.isupper())
+
+#FUNCION LEN: contar los caracteres de un string
+
+#cuenta el numero de cacteres de la variable frase
+print(len(frase))
+
+#cuenta el numero de caracteres de la variable frase2
+print(len(frase2))
+
+
+#suma el numero de caracteres de frase con frase2
+print(len(frase) + len(frase2))
+
+#transforma los caracteres de  frase y frase2 a mayusculas y compara si los caracteres de frase y frase2 son mayusculas, devuelve un valor booleano
+print(frase.upper().isupper() + frase2.upper().isupper())
+
+#finalmente suma el numero de caracteres de frase y frase2 junto con los valores booleanos de la comparaciòn de mayusculas de frase y frase2
+print(len(frase) + len(frase2) + frase.upper().isupper() + frase2.upper().isupper())
+
+
---
+++ variables/variablesytiposdedatos.py
@@ -0,0 +1,39 @@
+#Este programa imprime en pantalla el nombre del hermano de Alan
+#print("Ivan")
+
+#Este Comentario serà ignorado por Python
+
+#Palabras Reservadas
+# print("Palabra")
+# if
+# else
+# exec
+# await
+# for
+
+#Tipos de datos
+#"String o caracter" con comillas, tipo string
+#12 o puede ser 12.5, tipo numero
+#Isabel es Hombre? esHombre = False, tipo de datos booleano
+
+
+
+
+#Variables
+#Pequeña historia
+# print("Habia una vez un joven llamado Alan")
+# print("tenia 17 años")
+# print("a Alan le gusta ser llamado por su nombre: Alan")
+# print("pero el ya queria tener 18 años")
+
+#Pequeña historia con Variables
+
+nombre_de_personaje = "Alan"
+edad_de_personaje = "17"
+edad_deseada = "18"
+
+print("Habia una vez un joven llamado " + nombre_de_personaje + ",")
+print("tenia " + edad_de_personaje + " años")
+print("a Alan le gusta ser llamado por su nombre:" + nombre_de_personaje + ",")
+print("pero el ya queria tener " + edad_deseada + " años")
+
