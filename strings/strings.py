d0913ec7f2b8438e4154868e71a0a2580d7f72be
bmartinez <bryan.martinez@quetzalio.net>
2021-08-10 10:48:09 -0700

    funciones de index, posicion y reemplazo

--- strings/strings.py
+++ strings/strings.py
@@ -4,6 +4,7 @@
 # print("Este maestro es muy \"manchado\" ")
 
 frase = "El perico es verde"
+#        0123456789
 frase2 = " y come semillas"
 
 # print(frase + frase2)
@@ -32,20 +33,27 @@ frase2 = " y come semillas"
 
 #FUNCION LEN: contar los caracteres de un string
 
-#cuenta el numero de cacteres de la variable frase
-print(len(frase))
+# #cuenta el numero de cacteres de la variable frase
+# print(len(frase))
 
-#cuenta el numero de caracteres de la variable frase2
-print(len(frase2))
+# #cuenta el numero de caracteres de la variable frase2
+# print(len(frase2))
 
 
 #suma el numero de caracteres de frase con frase2
-print(len(frase) + len(frase2))
+# print(len(frase) + len(frase2))
 
-#transforma los caracteres de  frase y frase2 a mayusculas y compara si los caracteres de frase y frase2 son mayusculas, devuelve un valor booleano
-print(frase.upper().isupper() + frase2.upper().isupper())
+# #transforma los caracteres de  frase y frase2 a mayusculas y compara si los caracteres de frase y frase2 son mayusculas, devuelve un valor booleano
+# print(frase.upper().isupper() + frase2.upper().isupper())
 
-#finalmente suma el numero de caracteres de frase y frase2 junto con los valores booleanos de la comparaciòn de mayusculas de frase y frase2
-print(len(frase) + len(frase2) + frase.upper().isupper() + frase2.upper().isupper())
+# #finalmente suma el numero de caracteres de frase y frase2 junto con los valores booleanos de la comparaciòn de mayusculas de frase y frase2
+# print(len(frase) + len(frase2) + frase.upper().isupper() + frase2.upper().isupper())
 
+#variable[] rastrear la posiciòn de un caracter alfanumerico
+print(frase[9])
 
+#Variable.index() indica un caracter existente en la oraciòn y python devuelve la posicion
+# print(frase.index("z"))
+
+#variable.replace("palabra1 o caracter1", "palabra2 o caracter2") remplazarà la palabra indicada por otra
+# print(frase.replace("e","a"))
