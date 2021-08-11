57c20bb519b61c55f6c786b3542d560a71e76ad3
bmartinez <bryan.martinez@quetzalio.net>
2021-08-11 10:09:38 -0700

    calc

--- calculadora/calculadora.py
+++ calculadora/calculadora.py
@@ -1,11 +1,12 @@
 nombre = input("Escribe tu nombre: ")
-print("Bienvenido " + nombre + " al programa de calculadora")
+print("Bienvenido " + nombre + " al programa que suma numeros")
 print("Por favor escriba dos numeros a calcular")
-operacion = input("Escriba la operacion que desee realizar (+ o - o * o /): ")
 numero_a = input("Primer numero: ")
 numero_b = input("Segundo numero: ")
 
 
-result = float(numero_a) + float(operacion) + float(numero_b)
+result = float(numero_a) + float(numero_b)
 
 print("Resultado: ")
+
+print(result)
