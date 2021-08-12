068a9acfe4823eab421f566e2d17628f505c79b2
bmartinez <bryan.martinez@quetzalio.net>
2021-08-11 17:26:44 -0700

    se agregan listas y funciones de listas

---
+++ lists/lists.py
@@ -0,0 +1,28 @@
+#Con las listas podemos guardar multiples valores en una sola variable
+
+amigos = ["Alan", "Ivan", "Armando", "Jose Marcos"]
+
+# print(amigos)
+
+print(amigos[1])
+
+# #En una lista puedes almacenar numeros
+
+numeros = [1, 2, 3, 4, 5]
+#          0  1  2  3  4
+print(numeros[0,3])
+
+
+
+
+# #En una lista puedo almacenar numeros y strings
+
+# alfanumerico = [1, "Alan", 3, 24, "Ivan"]
+
+# print(alfanumerico)
+
+#Para mostrar de cierto dato a cierto dato se utiliza : donde el primer numero es donde empieza a mostrar el dato
+# y donde el segundo numero es hasta donde se va a mostrar el dato menos 1 posicion
+
+print(amigos[0:4])
+
