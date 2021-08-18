019c43d34fe8cd75a1e1c72d1bbf723aaad3a1c1
bmartinez <bryan.martinez@quetzalio.net>
2021-08-17 18:08:34 -0700

    termina delcaraciojn if

--- declaracion_if/declaracion_if.py
+++ declaracion_if/declaracion_if.py
@@ -70,4 +70,11 @@ elif carne=="si" and verdura=="no":
 else:
     print("Si no quiero carne pero si verdura, ordeno ensalada")
 
-
+#Comparadores logicos
+def num_mayor(num1, num2, num3):
+    if num1 >= num2 and num1 >= num3:
+        return num1
+    elif num2 >= num1 and num2 >= num3:
+        return num2
+    else:
+        num3
