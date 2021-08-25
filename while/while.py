f65b678b20c46a646f654a3f6ce3ef5356183f9f
bmartinez <bryan.martinez@quetzalio.net>
2021-08-25 15:10:01 -0700

    while

---
+++ while/while.py
@@ -0,0 +1,20 @@
+#While nos permite crear repeticiones y ciclos tantas veces como queramos. While ejecuta el codigo que se encuentra identado dentro de el
+#las veces que nosotros le indiquemos
+
+# i = 1
+
+# while i <= 10:
+#     print(i)
+#     i = i + 1
+
+#Podemos simplificar la operacion de sumatoria  de la siguiente forma
+
+i = 1
+
+while i <= 10:
+    print(i)
+    i += 1
+
+print("Termina loop")
+
+#Ejercicio, realizar un ejercicio donde
